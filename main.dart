d8e0812dbba4addca96271525c1a0d58efc0f646
JuergenKuck <drj.kuck@googlemail.com>
2025-03-05 10:54:18 +0100

    fertig

--- main.dart
+++ main.dart
@@ -1,3 +1,33 @@
-void main(){
-    
+enum Weekday { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
+
+void main() {
+  Ausgabe(Weekday.monday);
+  Ausgabe(Weekday.tuesday);
+  Ausgabe(Weekday.wednesday);
+  Ausgabe(Weekday.thursday);
+  Ausgabe(Weekday.friday);
+  Ausgabe(Weekday.saturday);
+  Ausgabe(Weekday.sunday);
+  Ausgabe(null);
+}
+
+void Ausgabe(Weekday? weekday) {
+  switch (weekday) {
+    case Weekday.monday:
+    case Weekday.tuesday:
+      print("Montage und Dienstage sind anstrengend");
+      break;
+    case Weekday.wednesday:
+    case Weekday.thursday:
+      print("Mittwoch und Donnerstag sind OK");
+      break;
+    case Weekday.friday:
+      print("Freitag ist super");
+      break;
+    case Weekday.saturday:
+    case Weekday.sunday:
+      print("Wochenende ist genial");
+    case null:
+      print("Kein Tag ausgewählt");
+  }
 }
